0eb525037427341a923442cfbfc018d8e27be17d
fassiliss <fassiliss@gmail.com>
2025-12-23 08:13:11 -0600

    Fix mobile menu - use template CSS classes

--- components/layout/MobileMenu.js
+++ components/layout/MobileMenu.js
@@ -11,18 +11,11 @@ const MobileMenu = ({ handleMobileMenu }) => {
         </div>
 
         <nav className="menu-box">
-          <div
-            className="nav-logo"
-            style={{ padding: "25px 20px", borderBottom: "1px solid #333" }}
-          >
-            <Link
-              href="/"
-              onClick={handleMobileMenu}
-              style={{ textDecoration: "none", display: "block" }}
-            >
+          <div className="nav-logo">
+            <Link href="/" onClick={handleMobileMenu}>
               <div
                 style={{
-                  fontSize: "28px",
+                  fontSize: "26px",
                   fontWeight: "bold",
                   color: "#DAA520",
                   lineHeight: "1.2",
@@ -32,24 +25,20 @@ const MobileMenu = ({ handleMobileMenu }) => {
               </div>
               <div
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   color: "#DAA520",
                   letterSpacing: "2px",
-                  marginTop: "5px",
-                  textTransform: "uppercase",
+                  marginTop: "3px",
                 }}
               >
-                Ethio-Italian Cuisine
+                ETHIO-ITALIAN CUISINE
               </div>
             </Link>
           </div>
           <div className="menu-outer">
-            <div
-              className="collapse navbar-collapse show clearfix"
-              id="navbarSupportedContent"
-            >
-              <ul className="navigation">
-                <li>
+            <div className="collapse navbar-collapse show clearfix">
+              <ul className="navigation clearfix">
+                <li className="current">
                   <Link href="/" onClick={handleMobileMenu}>
                     Home
                   </Link>
@@ -78,53 +67,30 @@ const MobileMenu = ({ handleMobileMenu }) => {
             </div>
           </div>
 
-          <div
-            style={{
-              padding: "25px 20px",
-              borderTop: "1px solid #333",
-              marginTop: "20px",
-            }}
-          >
-            <div
-              style={{
-                color: "#DAA520",
-                fontSize: "14px",
-                marginBottom: "15px",
-              }}
-            >
-              Order Online, Text or Call
-            </div>
-            <a
-              href="tel:6155699687"
-              style={{
-                display: "inline-block",
-                padding: "12px 25px",
-                backgroundColor: "#DAA520",
-                color: "#1a1a1a",
-                fontSize: "16px",
-                fontWeight: "bold",
-                borderRadius: "50px",
-                textDecoration: "none",
-              }}
-            >
-              📞 615-569-9687
-            </a>
+          <div className="contact-info">
+            <h4>Order Online, Text or Call</h4>
+            <ul>
+              <li>
+                <a href="tel:6155699687">📞 615-569-9687</a>
+              </li>
+              <li>Nashville, Tennessee</li>
+              <li>Pickup & Delivery Available</li>
+            </ul>
           </div>
 
-          <div style={{ padding: "15px 20px" }}>
-            <a
-              href="#"
-              style={{
-                color: "#DAA520",
-                fontSize: "20px",
-                marginRight: "15px",
-              }}
-            >
-              <i className="fa fa-facebook"></i>
-            </a>
-            <a href="#" style={{ color: "#DAA520", fontSize: "20px" }}>
-              <i className="fa fa-instagram"></i>
-            </a>
+          <div className="social-links">
+            <ul className="clearfix">
+              <li>
+                <a href="#">
+                  <span className="fab fa-facebook-f"></span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="fab fa-instagram"></span>
+                </a>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
